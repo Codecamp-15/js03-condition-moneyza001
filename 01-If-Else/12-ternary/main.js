c2e43bd3409370111dc9a91301e2fc9738227cf1
moneyza001 <supalap.ss@gmail.com>
2023-08-08 17:43:32 +0700

    Finnished la012

--- 01-If-Else/12-ternary/main.js
+++ 01-If-Else/12-ternary/main.js
@@ -1,24 +1,6 @@
-let number = prompt('Put your point')
-let grade ;
-
-if(number === null || number.trim()=== '' || isNaN(number)=== true){
-    alert('invalid number')
-}
-else if(number >= 80 ) {
-    alert('A')
-}
-else if(number >= 70 && number < 80 ) {
-    alert('B')
-}
-else if(number >= 60 && number < 70 ) {
-    alert('C')
-}
-else if(number >= 50 && number < 60 ) {
-    alert('D')
-}
-else if(number >= 0 && number < 50) {
-    alert('F')
-}
-else if(number < 0) {
-    alert('Invalid number')
-}
+let points = +prompt()
+points >= 80 ? alert('a'):
+points > 70 ?  alert('b'):
+points > 60 ?  alert('c'):
+points > 50 ?  alert('d'):
+alert('f');
